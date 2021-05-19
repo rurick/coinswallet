2e9b5c099d0e2fab25a3f260bc31ec535faa2706
rurick <rurick80@gmail.com>
2021-05-19 23:42:01 +0300

    +Add offset,limit parametrs to list services +Add main package

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,3 +1,6 @@
+// Copyright 2021 (c) Yuriy Iovkov aka Rurick.
+// yuriyiovkov@gmail.com; telegram: @yuriyiovkov
+
 package service
 
 import (
@@ -7,14 +10,28 @@ import (
 )
 
 type Service interface {
+
 	// Send - send amount of currency between two wallet accounts. Return transaction id if success or error
 	Send(ctx context.Context, from entity.AccountName, to entity.AccountName, amount float64) (entity.Payment, error)
+
 	// Deposit - deposit amount of currency to the wallet account.  Return transaction id if success or error
 	Deposit(ctx context.Context, to entity.AccountName, amount float64) (entity.Payment, error)
+
 	// CreateAccount -  create new wallet account
 	CreateAccount(ctx context.Context, name entity.AccountName) error
-	// PaymentsList - list of payments of user
-	PaymentsList(ctx context.Context, name entity.AccountName) ([]entity.Payment, error)
-	// AccountsList - List of all registred accounts
-	AccountsList(ctx context.Context) ([]entity.AccountName, error)
+
+	// PaymentsList - list of payments of user.
+	// if set offset and limit > 0 returns slice
+	// if limit =-1 returns all payments
+	PaymentsList(ctx context.Context, name entity.AccountName, offset, limit int64) ([]entity.Payment, error)
+
+	// AllPaymentsList - list of all payments
+	// if set offset and limit > 0 returns slice
+	// if limit =-1 returns all payments
+	AllPaymentsList(ctx context.Context, name entity.AccountName, offset, limit int64) ([]entity.Payment, error)
+
+	// AccountsList - List of all registered accounts
+	// if set offset and limit > 0 returns slice
+	// if limit =-1 returns all accounts
+	AccountsList(ctx context.Context, offset, limit int64) ([]entity.AccountName, error)
 }
