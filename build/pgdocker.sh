94f76f3ba5f40fa4e61393eb43cd3ae8dc01388a
Юрий Иовков <y.iovkov@digital-kitchen.ru>
2021-05-21 15:49:23 +0300

    instruction to readme file

--- build/pgdocker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-# run docker container with the postgresql
-
-sudo docker run --rm --name coins-pgdocker -e POSTGRES_PASSWORD=coins -e POSTGRES_USER=coins -e POSTGRES_DB=coins -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres
