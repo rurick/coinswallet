e1ebfde7dfed33a8bc64922b59f83712bf23cc53
rurick <rurick80@gmail.com>
2021-05-24 05:59:57 +0300

    upgrade docker compose file

--- build/test.sh
+++ build/test.sh
@@ -7,13 +7,16 @@ export PGSQL_HOST=127.0.0.1
 export PGSQL_NAME=coins
 export PGSQL_USER=coins
 export PGSQL_PASS=coins
-export PGSQL_PORT=5433
+export PGSQL_PORT=5432
 export CacheExpTime=10
 
-docker run --rm --name coins-pgdocker -e POSTGRES_PASSWORD=coins -e POSTGRES_USER=coins -e POSTGRES_DB=coins -d -p 5433:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres
+cd $ROOT_DIR/build
+
+docker-compose up -d
 
 go clean -testcache
 go test $ROOT_DIR/pkg/...
 go test $ROOT_DIR/internal/...
 
-docker stop coins-pgdocker
+docker-compose down
+
