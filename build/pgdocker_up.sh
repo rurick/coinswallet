5c80dbe11e9720458680fb2b95299291eed37cf2
rurick <rurick80@gmail.com>
2021-05-24 07:08:14 +0300

    add int.sql

--- build/pgdocker_up.sh
+++ build/pgdocker_up.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 docker run --rm --name coins-pgdocker -e POSTGRES_PASSWORD=coins -e POSTGRES_USER=coins -e POSTGRES_DB=coins -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres:10
+PGPASSWORD=coins psql -hlocalhost -p5432 -Ucoins -dcoins -f init.sql
